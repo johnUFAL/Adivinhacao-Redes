0c7afb9c4a8c69a42bf093ce2a1de37259b26a2e
johnUFAL <jvdn@ic.ufal.br>
2025-10-24 14:48:36 -0300

    Adição do número global e melhora na resposta do servidor

--- cliente.py
+++ cliente.py
@@ -1,18 +1,28 @@
-# Cliente python
+#conectar, escutar, enviar, inicar cliente, inicar thread 
 import socket
 import threading
 import time
 from protocolo import Protocolo
 
-#conectar, escutar, enviar, inicar cliente, inicar thread 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(('localhost', 8888))
 
-mensagem = Protocolo.codificar(Protocolo.TENTATIVA, "10")
-cliente.send(mensagem.encode())
-
-resp = cliente.recv(1024).decode()
-comando, dados = Protocolo.decodificar(resp)
-print(f'Server resposta: {comando} | {dados}')
+while True:
+    tentativa = input('Num ou sair: ')
+    if tentativa.lower() == 'sair':
+        time.sleep(.2)
+        cliente.send(Protocolo.codificar(Protocolo.SAIR, "").encode())
+        break
+    
+    time.sleep(.2)
+    cliente.send(Protocolo.codificar(Protocolo.TENTATIVA, tentativa).encode())
+    
+    resp = cliente.recv(1024).decode()
+    if not resp:
+        print('Server fechado.')
+        break
+    
+    comando, dados = Protocolo.decodificar(resp)
+    print(f'Server resposta: {comando} | {dados}')
 
 cliente.close()
--- servidor.py
+++ servidor.py
@@ -1,15 +1,30 @@
-# Servidor python
+#Inicar server, inicar jogo, notificar, remover desconectados, receber dados, criar thread 
 import socket 
 import threading
 import random
+import time
 from protocolo import Protocolo
 
-#Inicar server, inicar jogo, notificar, remover desconectados, receber dados, criar thread 
+# prpriedades do jogo e inicalização de partida
+class Jogo:
+    def __init__(self):
+        self.num_secreto = None
+        self.jogo_ativo = False
+        self.clientes = [] #lita de clientes
+
+    def inicar_game(self):
+        self.num_secreto = random.randint(1, 100) #gerando numero aleatorio
+        self.jogo_ativo = True
+        print(f'Nova partida, Num: {self.num_secreto}')
+
+#global game aqui
+jogo = Jogo()
+
 #multiplos clientes
 def clientes(conexao, endereco):
     print(f'[Nova conexão] cliente conectado em {endereco}')
 
-    num_secreto = random.randint(1, 100)
+    time.sleep(0.2)  
     conexao.send(Protocolo.codificar(Protocolo.INICIAR, "Adivinhe o n. escolhido entre 1 e 100").encode())
 
     while True:
@@ -20,19 +35,23 @@ def clientes(conexao, endereco):
             
             comando, dados = Protocolo.decodificar(msg)
             if comando == Protocolo.SAIR:
+                time.sleep(0.2)  
                 conexao.send(Protocolo.codificar(Protocolo.FIM_PARTIDA, "Saindo do jogo...").encode())
                 break
 
             elif comando == Protocolo.TENTATIVA:
-                ttv = int(dados)
-                if ttv > num_secreto:
+                tentativa = int(dados)
+                if tentativa > jogo.num_secreto:
+                    time.sleep(0.2)  
                     conexao.send(Protocolo.codificar(Protocolo.MAIOR, "O numero é menor").encode())
-                elif ttv < num_secreto:
+                elif tentativa < jogo.num_secreto:
+                    time.sleep(0.2)  
                     conexao.send(Protocolo.codificar(Protocolo.MENOR, "O numero é maior").encode())
                 else:
+                    time.sleep(0.2)  
                     conexao.send(Protocolo.codificar(Protocolo.ACERTOU, "Voceê acertou!!!").encode())
-                    break
             else:
+                time.sleep(0.2)  
                 conexao.send(Protocolo.codificar(Protocolo.ERRO, "Comando inválido").encode())
         
         except Exception as e:
@@ -50,6 +69,8 @@ def main():
     #agaurda conexão
     servidor.listen()
     print('[server ativado]')
+
+    jogo.inicar_game()
 
     while True:
         conexao, endereco = servidor.accept()
