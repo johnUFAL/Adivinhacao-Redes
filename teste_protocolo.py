f25dfa430270eb61ce530ffafb02051594ed22a4
Gui <guilhermealessander9@gmail.com>
2025-10-28 18:41:57 -0300

    organizacao das pastas

--- teste_protocolo.py
+++
@@ -1,7 +0,0 @@
-from protocolo import Protocolo
-
-mensagem = Protocolo.codificar(Protocolo.ACERTOU, "Você ganhou!")
-print(f'Msg codificada: {mensagem}')
-
-comando, dados = Protocolo.decodificar(mensagem)
-print(f'Comando: {comando}, Dados: {dados}')
